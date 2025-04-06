44c7ddaacb129df1204db7c9934ca929dc74c0f6
KreconyMakaron <55319736+KreconyMakaron@users.noreply.github.com>
2025-04-06 20:26:15 +0200

    feat: added osd

--- app.ts
+++ app.ts
@@ -1,6 +1,7 @@
 import { App } from "astal/gtk3"
 import style from "./style.scss"
 import Bar from "./widget/bar"
+import OSD from "./widget/osd"
 import ControlCenter from  "./widget/controlcenter"
 
 App.start({
@@ -8,5 +9,6 @@ App.start({
   css: style,
   main: () => {
     App.get_monitors().map(Bar)
+    App.get_monitors().map(OSD)
   }
 })
