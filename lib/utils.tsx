7fe16808c208614de01b834bdc8f47c6d9842849
KreconyMakaron <55319736+KreconyMakaron@users.noreply.github.com>
2025-04-06 20:25:08 +0200

    refactor: made a lib dir

---
+++ lib/utils.tsx
@@ -0,0 +1,77 @@
+import Wp, { AstalWpEndpoint } from "gi://AstalWp"
+import { Variable, bind } from "astal"
+import Brightness from "./brightness"
+import GObject, { register, property } from "astal/gobject"
+
+function getVolumeIcon(speakerObj: AstalWpEndpoint) {
+  if(speakerObj.mute == true) return "volume-x-symbolic"
+  if(speakerObj.volume < .3) return "volume-0-symbolic"
+  if(speakerObj.volume < .6) return "volume-1-symbolic"
+  else return "volume-2-symbolic"
+}
+
+function getBrightessIcon(brightness: Brightness) {
+  if(brightness.screen < .3) return "brightness-low-symbolic"
+  if(brightness.screen < .6) return "brightness-half-symbolic"
+  else return "brightness-high-symbolic"
+}
+
+@register({ GTypeName: "DynamicIcon" })
+export default class DynamicIcon extends GObject.Object {
+  static instance: DynamicIcon
+  static get_default() {
+    if (!this.instance) this.instance = new DynamicIcon()
+    return this.instance
+  }
+
+  #speakerObj = Wp.get_default()!.get_default_speaker()
+  #brightObj = Brightness.get_default()
+
+  #volume = getVolumeIcon(this.#speakerObj)
+  #brightness = getBrightessIcon(this.#brightObj)
+  #recent = this.#volume
+  #popup = this.#volume
+
+  @property(String)
+  get volume() { return this.#volume }
+
+  @property(String)
+  get brightness() { return this.#brightness }
+
+  @property(String)
+  get recent() { return this.#recent }
+
+  @property(String)
+  get popup() { return this.#popup }
+
+  constructor() {
+    super()
+    this.#speakerObj.connect("notify::volume", () => {
+      this.#volume = getVolumeIcon(this.#speakerObj)
+      this.#recent = this.#volume
+      this.notify("volume")
+      this.notify("recent")
+    })
+
+    this.#speakerObj.connect("notify::mute", () => {
+      this.#volume = getVolumeIcon(this.#speakerObj)
+      this.#recent = this.#volume
+      this.#popup = this.#volume
+      this.notify("volume")
+      this.notify("recent")
+      this.notify("popup")
+    })
+
+    this.#brightObj.connect("notify::screen", () => {
+      this.#brightness = getBrightessIcon(this.#brightObj)
+      this.#recent = this.#brightness
+      this.notify("brightness")
+      this.notify("recent")
+    })
+
+    this.#brightObj.connect("notify::kbd", () => {
+      this.#popup = "power-symbolic"
+      this.notify("popup")
+    })
+  }
+}
