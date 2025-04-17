e9b62e7029af0184e4cbc118c3a01a57055836ba
KreconyMakaron <55319736+KreconyMakaron@users.noreply.github.com>
2025-04-17 14:20:47 +0200

    a

--- flake.nix
+++ flake.nix
@@ -30,7 +30,7 @@
       default = ags.lib.bundle {
         inherit pkgs;
         src = ./.;
-        name = "my-shell";
+        name = "kreshell";
         entry = "app.ts";
 
         # additional libraries and executables to add to gjs' runtime
